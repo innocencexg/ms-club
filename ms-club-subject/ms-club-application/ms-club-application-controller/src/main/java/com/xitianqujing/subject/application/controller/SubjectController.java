971aaa03b831bae6279f4ba64fcbf3d5661dec00
m0_64306962 <m0_64306962@gitcode.net>
2024-04-16 15:00:40 +0800

    ms-club-subject  controller测试

--- ms-club-subject/ms-club-application/ms-club-application-controller/src/main/java/com/xitianqujing/subject/application/controller/SubjectController.java
+++ ms-club-subject/ms-club-application/ms-club-application-controller/src/main/java/com/xitianqujing/subject/application/controller/SubjectController.java
@@ -1,5 +1,6 @@
 package com.xitianqujing.subject.application.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,4 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class SubjectController {
+    @GetMapping("/test")
+    public String test(){
+        return "hello";
+    }
 }
