24443caf708820a614690327065be01cd47ee638
m0_64306962 <m0_64306962@gitcode.net>
2024-05-05 14:31:35 +0800

    --task=1023650 --user=高翔        feat:es接入

---
+++ ms-club-subject/ms-club-infra/src/main/java/com/xitianqujing/subject/infra/basic/entity/SubjectInfoEs.java
@@ -0,0 +1,34 @@
+package com.xitianqujing.subject.infra.basic.entity;
+import com.xitianqujing.subject.common.entity.PageInfo;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+
+/**
+ * @Author: gx
+ * @CreateTime: 2024/04/18  18:52
+ */
+@Data
+public class SubjectInfoEs extends PageInfo implements Serializable {
+
+    private Long subjectId;
+
+    private Long docId;
+
+    private String subjectName;
+
+    private String subjectAnswer;
+
+    private String createUser;
+
+    private Long createTime;
+
+    private Integer subjectType;
+
+    private String keyWord;
+
+    private BigDecimal score;
+
+}
